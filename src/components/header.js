cd8c20274a7fec4695a1e4682da5f57c053b1463
yayano <boussetta13yahia@gmail.com>
2021-03-02 22:04:05 +0100

    create navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 import {
   Collapse,
   Navbar,
@@ -10,30 +10,33 @@ import {
   NavLink,
   NavbarText,
 } from "reactstrap"
-const [isOpen, setIsOpen] = useState(false)
-const toggle = () => setIsOpen(!isOpen)
-const Header = ({ siteTitle }) => (
-  <Navbar color="light" light expand="md">
-    <div className="container">
-      <NavbarBrand href="/">{siteTitle}</NavbarBrand>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="mr-auto" navbar>
-          <NavItem>
-            <NavLink href="/team">Team</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/tags">Tags </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/about">About </NavLink>
-          </NavItem>
-        </Nav>
-        <NavbarText>Simple Text</NavbarText>
-      </Collapse>
-    </div>
-  </Navbar>
-)
+
+const Header = ({ siteTitle }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
+  return (
+    <Navbar color="light" light expand="md">
+      <div className="container">
+        <NavbarBrand href="/">{siteTitle}</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={!isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/team">Team</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/tags">Tags </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about">About </NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </div>
+    </Navbar>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
